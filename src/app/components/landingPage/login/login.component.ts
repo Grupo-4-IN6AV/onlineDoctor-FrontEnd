670bb1576bc425c1d3d9ce41fdea7d59e90f970d
Sduard Ixpatac <sixpatac-2020209@kinal.edu.gt>
2022-07-21 00:26:30 -0600

    Se agregó -Login- y -Register-.

--- src/app/components/landingPage/login/login.component.ts
+++ src/app/components/landingPage/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 import { UserModel } from 'src/app/models/user.model';
 import { CredentialsRestService } from 'src/app/services/credentialsRest/credentials-rest.service';
+import {ScriptsLoginService} from 'src/app/services/cargarScripts/scripts-login.service'
 
 @Component({
   selector: 'app-login',
@@ -12,13 +13,19 @@ import { CredentialsRestService } from 'src/app/services/credentialsRest/credent
 export class LoginComponent implements OnInit
 {
   user:  UserModel;
-
+  confirmPassword : string = '';
+  timer : any;
+  
   constructor
   (
     private router: Router,
     private userRest: CredentialsRestService,
-  ) {
-    this.user = new UserModel('', '', '', '', '', '', '', '', true);
+    private _ScriptsLogin: ScriptsLoginService
+    )
+    {
+    
+    this.user = new UserModel('', '', '', '', '', '', '', '', true, '','');
+    _ScriptsLogin.Carga(["app"]);
   }
 
   ngOnInit(): void {
@@ -29,17 +36,17 @@ export class LoginComponent implements OnInit
   {
     this.userRest.login(this.user).subscribe({
       next: (res:any)=>{
-        localStorage.setItem('identity', JSON.stringify(res.already));
+        localStorage.setItem('identity', JSON.stringify(res.userExist));
         localStorage.setItem('token', res.token);
 
         Swal.fire({
           icon:'success',
           title: res.message,
-          html:'Welcome <b>'+ res.already.username+'</b>',
+          html:'Welcome <b>'+ res.userExist.username+'</b>',
           confirmButtonColor: '#28B463'
         })
 
-        const verificarAdmin = res.already.role;
+        const verificarAdmin = res.userExist.role;
         //VERIFICA A DONDE LLEVARME//
         if(verificarAdmin == 'ADMIN')
          {this.router.navigate(['/admin/home']);}
@@ -52,5 +59,66 @@ export class LoginComponent implements OnInit
         });
       },
     })
+  }
+
+
+  async checkPassword()
+  {
+    clearTimeout(this.timer);
+    this.timer = await setTimeout(()=>
+    {
+      if(this.user.password != '')
+      {
+        if (this.confirmPassword != this.user.password)
+        {
+          Swal.fire({
+            icon:'error',
+            title: 'Password do not Match',
+            html:'Try Again',
+            confirmButtonColor: '#E74C3C'
+          })
+        }
+        else
+        {
+          Swal.fire({
+            icon:'success',
+            title: 'Passwords Match',
+            confirmButtonColor: '#28B463'
+          })
+        }
+      }
+      else
+      {
+        Swal.fire({
+          icon:'info',
+          title: 'Set value in input Password',
+          confirmButtonColor: '#0D6EFD'
+        })
+      }
+    }, 800);
+  }
+
+  register()
+  {
+    this.userRest.register(this.user).subscribe
+    ({
+
+      next : (res : any) =>
+      {
+        Swal.fire({
+          title: res.message,
+          html:'Already can Login now.',
+          confirmButtonColor: '#28B463'
+        })
+        this.router.navigateByUrl('/')
+      },
+      error: (err: any) => {
+        Swal.fire({
+          icon: 'error',
+          title: err.error.message || err.error,
+          confirmButtonColor: '#E74C3C'
+        });
+      },
+    });
   }
 }
