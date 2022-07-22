ecae99da5c1e3d0a2444faa5c750ab095799c945
mlopez-2020202 <mlopez-2020202@kinal.edu.gt>
2022-07-21 19:35:09 -0600

    Se agregó -Type Laboratory-

---
+++ src/app/services/typeLaboratoryRest/type-laboratory-rest.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { CredentialsRestService } from '../credentialsRest/credentials-rest.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TypeLaboratoryRestService {
+
+  httpOptions = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': this.credentialReset.getToken(),
+  });
+
+  constructor(
+    private credentialReset: CredentialsRestService,
+    private http: HttpClient,
+  ) { }
+
+  getTypesLaboratory()
+  {
+    return this.http.get(environment.baseURI + 'typeLaboratory/getTypesLaboratory', { headers: this.httpOptions });
+  }
+
+  saveTypeLaboratory(params:{})
+  {
+    return this.http.post(environment.baseURI + 'typeLaboratory/saveTypeLaboratory', params, { headers: this.httpOptions });
+  }
+
+  getTypeLaboratory(id:string)
+  {
+    return this.http.get(environment.baseURI + 'typeLaboratory/getTypeLaboratory/' + id, { headers: this.httpOptions });
+  }
+
+  updateTypeLaboratory(id:string, params:{})
+  {
+    return this.http.put(environment.baseURI + 'typeLaboratory/updateTypeLaboratory/' + id, params, { headers: this.httpOptions });
+  }
+
+  deleteTypeLaboratory(id:string)
+  {
+    return this.http.delete(environment.baseURI + 'typeLaboratory/deleteTypeLaboratory/' + id, { headers: this.httpOptions });
+  }
+}
