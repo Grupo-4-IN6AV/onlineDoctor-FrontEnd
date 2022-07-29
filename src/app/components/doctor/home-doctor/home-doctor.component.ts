fd944799530b62f003e062ddeb9ec106b5fd9ce4
Javier Linares <jlinares-2020162@kinal.edu.gt>
2022-07-29 02:18:26 -0600

    Se agregó el home de doctores.

--- src/app/components/doctor/home-doctor/home-doctor.component.ts
+++ src/app/components/doctor/home-doctor/home-doctor.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-doctor',
   templateUrl: './home-doctor.component.html',
-  styleUrls: ['./home-doctor.component.sass']
+  styleUrls: ['./home-doctor.component.css']
 })
 export class HomeDoctorComponent implements OnInit {
 
