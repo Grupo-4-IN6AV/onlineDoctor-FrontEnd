9e556cc86c3d43573ea081811d6f61382a0ba84e
Sduard Ixpatac <sixpatac-2020209@kinal.edu.gt>
2022-07-29 11:05:05 -0600

    Se corrigieron errores

---
+++ src/app/services/billRest/bill-rest.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { CredentialsRestService } from '../credentialsRest/credentials-rest.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BillRestService {
+  httpOptions = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': this.credentialReset.getToken(),
+  });
+
+  constructor(
+    private credentialReset: CredentialsRestService,
+    private http: HttpClient,
+  ) { }
+
+  createBill(){
+    return this.http.post(environment.baseURI + 'bill/createBill', { headers: this.httpOptions.set('Authorization', this.credentialReset.getToken()) });
+  }
+}
---
+++ src/app/services/shoppingCartRest/shopping-cart-rest.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { CredentialsRestService } from '../credentialsRest/credentials-rest.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingCartRestService {
+  httpOptions = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': this.credentialReset.getToken(),
+  });
+
+  constructor(
+    private credentialReset: CredentialsRestService,
+    private http: HttpClient,
+  ) { }
+
+  createShoppingCart(params: {}){
+    return this.http.post(environment.baseURI + 'shoppingCart/createShoppingCart', params, { headers: this.httpOptions.set('Authorization', this.credentialReset.getToken()) });
+  }
+}
